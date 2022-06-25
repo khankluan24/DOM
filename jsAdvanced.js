46b209d88ac3acc82cb0b6959ebf624083e2cc38
=LongPro <longtran24062000@gmail.com>
2022-06-25 19:49:13 +0700

    Hoc IIFE

---
+++ jsAdvanced.js
@@ -0,0 +1,23 @@
+// 1. IIFE
+/**
+ * IIFE == Self-invoking function
+ const app = (function() {
+     // Private giong OOP
+     let cars = [];
+     return {
+         get(index) {
+             return cars[index];
+         },
+         add(car) {
+             cars.push(car)
+         },
+         edit(index, car) {
+              cars[index] = car
+         },
+         delete(index) {
+             cars.splice(index, 1)
+         }
+     }
+ }())
+ */
+
