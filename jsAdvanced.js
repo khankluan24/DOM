2bb1cbfc4ae41dfa6c52c3b3e7b4ef68d1829a76
long <longtran24062000@gmail.com>
2022-06-26 15:34:58 +0700

    Update...

--- jsAdvanced.js
+++ jsAdvanced.js
@@ -20,5 +20,70 @@
      }
  }())
  */
-// 2. Scope
 
+
+// 2. Scope
+/**
+  - Biến trong func đc tham chiếu bởi 1 func khác 
+  function logger() {
+      let counter = 0;
+      function increase() {
+          return ++counter;
+      }
+      return increase;
+  }
+  const logger1 = logger();
+  console.log(logger1()); //1
+  console.log(logger1()); //2 
+  console.log(logger1()); //3
+ * 
+ */
+
+// 3. Closure 
+/**
+ function createCounter() {
+     let counter = 0;
+     function increase() {
+         return ++counter;
+     }
+     return increase;
+ }
+ const counter1 = createCounter();
+ console.log(counter1()); //1
+ console.log(counter1()); //2
+
+
+
+ function createLogger(namespace) {
+     function logger(message) {
+         console.log(`[${namespace}] ${message}`)
+     }
+     return logger
+ }
+ // =============== APP ===============
+ 
+ // Register.js 
+ const infoLogger = createLogger('Info')
+ 
+ infoLogger('Bat dau goi mail');
+ infoLogger('Triển khai gởi mail tiếp theo');
+ 
+ // ForgotPassword.js 
+ const errorLogger = createLogger('Error')
+ 
+ errorLogger('Bat dau goi mail');
+ errorLogger('Triển khai gởi mail tiếp theo');
+ */
+function createApp() {
+    const cars = [];
+
+    return {
+        add(car) {
+            cars.push(car);
+        },
+        show() {
+            console.log(cars)
+        }
+    }
+}
+const app = createApp()
--- scope.md
+++ scope.md
@@ -1,3 +1,32 @@
 # IIFE (Immediately invoked function expression)
-# Scope - Pham vi
-- Cac loai pham vi vi vi i ddd
+# Scope - Phạm vi
+- Cac loại phạm vi:
+  - Global : Khi khai báo ngoài code block **dù cho là var, const ,let, function**
+  - Code block - Khoi ma: let,const nhung **var sẽ lấy phạm vi bên ngoài gần nhất** => Vd: `{  
+    var age = 18`;
+} thì age sẽ lấy phạm vi global
+    
+  - Local scope - Ham: var, function
+=> VD: `function logger() {
+    function logger2() {
+        console.log('Log 2');
+    }  
+    logger2()
+}
+logger() // Log 2`
+- Khi gọi mỗi hàm, mới tạo ra 1 phạm vi
+- Các func có thể access biến được khai báo trong phạm vi của nó và bên ngoài nó
+- Cách thức 1 biến được truy cập
+- Khi nào 1 biến bị xóa khỏi bộ nhớ ?
+  - Global: Khi thoát chương trình 
+  - Code block & function: Khi di chuyển ra ngoài phạm vi
+  - Biến trong func đc tham chiếu bởi 1 func khác 
+
+# Closure
+Là 1 hàm có thể ghi nhớ nơi nó được tạo ra và có thể truy cập biến bên ngoài nó
+#### Ứng dụng:
+- Viết code ngắn 
+- Biểu diễn, ứng dụng tính Private trong OOP
+#### Lưu ý:
+- Biến được tham chiếu trong closure sẽ không được xóa khóa bộ nhớ khi hàm cha thực thi xong 
+- Các khái niệm JS nâng cao dễ gây nhầm lẫn
